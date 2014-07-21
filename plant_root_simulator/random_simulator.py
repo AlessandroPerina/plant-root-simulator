a6abca0c8242d909acd69651a42f5e782fb76a3b
AlessandroPerina <alessandro.perina@gmail.com>
2014-07-21 21:36:41 +0200

    random simulator

---
+++ plant_root_simulator/random_simulator.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This temporary script file is located here:
+C:\Users\APerina\.spyder2\.temp.py
+"""
+from scipy.random import random
+import numpy as np
+from matplotlib import pyplot as pp
+
+steps = 1000
+x=np.zeros([steps])
+y=np.zeros([steps])
+vx=np.ones([steps])
+vy=np.ones([steps])
+eta_x = 1
+eta_y = 1
+for i in range(1,steps):
+    vy[i] = vy[i-1] + eta_y*(random(1)/2)
+    vx[i] = 0.2*vx[i-1] + eta_x*(random(1)-0.5)
+    y[i] = y[i-1] - vy[i]
+    x[i] = x[i-1] + vx[i]
+
+
+    
+pp.plot(x,y,'o')
+
+    
+    
