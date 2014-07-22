916f2d071decf58249259c55611d4b4a3c6cb40d
AlessandroPerina <alessandro.perina@gmail.com>
2014-07-22 11:50:03 +0200

    v0.1 - Sto scrivendo la classe root system

--- plant_root_simulator/root_system.py
+++ plant_root_simulator/root_system.py
@@ -1,3 +1,25 @@
+import root as R
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as pp
+
 class root_system:
 
-	
+	def __init__(self, rootList):
+		self.roots = rootList
+		self.no_roots = len(rootList)
+		self.curr_step = 0
+
+	def step(self,step=1):
+		for i in range(0,self.no_roots):
+			rantmp = sp.random.random(1).squeeze()
+			self.roots[i].x[self.roots[i].apex+1] = rantmp
+			self.roots[i].y[self.roots[i].apex+1] = i	
+			print "Root nr " + str(i) + " updated! New position " + str(rantmp)
+
+	def show_system(self):
+
+					
+
+
+
--- plant_root_simulator/simulator.py
+++ plant_root_simulator/simulator.py
@@ -1,10 +1,19 @@
 import root as R
+import root_system as RS
 import numpy as np
 import scipy as sp
 from matplotlib import pyplot as pp
 
-root1 = R.root(0,1,500) 
 
-pp.figure()
-pp.show()
-R.show_trajectory()
+roots = list()
+no_roots = 10
+for i in range(0,no_roots):
+	start = sp.random.random(1)
+	tmp_root = R.root(start,1,500)
+	roots.append( tmp_root)
+	print "Appended!" + " starting position " + str(start)
+
+print "No of roots " + str(len(roots))
+
+rootSys = RS.root_system( roots )
+rootSys.step()
